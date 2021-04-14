9a4bc0db619a418ef2523e0824fb883b7fec65f2
Marquis <Marquis.jayshaun.jarman@gmail.com>
2021-04-14 08:58:56 -0400

    Homework

---
+++ app.js
@@ -0,0 +1,17 @@
+var firstNum = 10;
+var secondNum = 5;
+
+function addTwo(){
+    sum = firstNum + secondNum;
+}
+
+function timesTwo(){
+    answer = firstNum*secondNum;
+}
+
+function divideTwo(){
+    answer = firstNum/secondNum;
+}
+function subtractTwo(){
+    answer= firstnum-secondNum;
+}
